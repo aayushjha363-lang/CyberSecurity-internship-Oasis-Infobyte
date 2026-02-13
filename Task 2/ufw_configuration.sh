75e273f5d5bba94c79c298f756d85b9cdec3d4eb
aayushjha363-lang <aayushjha363@gmail.com>
2026-02-13 20:13:41 +0530

    Configure UFW with default rules and SSH access

--- Task 2/ufw_configuration.sh
+++ Task 2/ufw_configuration.sh
@@ -1,1 +1,11 @@
+sudo apt update
+sudo apt install ufw -y
 
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+
+sudo ufw allow ssh
+sudo ufw deny http
+
+sudo ufw enable
+sudo ufw status verbose
